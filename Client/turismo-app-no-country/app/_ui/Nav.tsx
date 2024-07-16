638fd1b8753ab657f199b9f0f9e5938f2b2cfcb4
Jorge Villafraz <jorge9372@gmail.com>
2024-07-15 22:21:58 -0400

    feat: add project structure

---
+++ Client/turismo-app-no-country/app/_ui/Nav.tsx
@@ -0,0 +1,7 @@
+export const Nav = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-3xl">Nav desde ui</h1>
+        </div>
+    );
+};
--- Client/turismo-app-no-country/app/page.tsx
+++ Client/turismo-app-no-country/app/page.tsx
@@ -3,10 +3,17 @@ import { Button } from '@mui/material';
 export default function Home() {
     return (
         <main>
-            <h1>Nuevo proyecto</h1>
-            <Button variant="contained" color="primary" className='bg-red-400 rounded-3xl'>
-                Hola mundo desde MUI
-            </Button>
+            <div className='flex flex-row'>
+                <h1>Nuevo proyecto</h1>
+                <p className="text-blue-400">Este es un proyecto de turismo</p>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className="bg-red-100"
+                >
+                    Hola mundo desde MUIIIIIII
+                </Button>
+            </div>
         </main>
     );
 }
