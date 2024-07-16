c340b9347a68c276422a18cdc2ff2e5438c2ed10
Gabriel <loveragabriel20@gmail.com>
2024-07-16 20:14:17 -0300

    agrego tabla Place

---
+++ My-Route/src/main/java/com/myroute/entity/City.java
@@ -0,0 +1,15 @@
+package com.myroute.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+}
---
+++ My-Route/src/main/java/com/myroute/entity/Place.java
@@ -0,0 +1,39 @@
+package com.myroute.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private City city;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+}
