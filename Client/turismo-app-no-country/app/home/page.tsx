fa7f7769b30b4d224ac9bf187a511613c5d46518
Jorge Villafraz <jorge9372@gmail.com>
2024-07-16 22:53:23 -0400

    feat: Add ImageCarousel component for displaying image slides

--- Client/turismo-app-no-country/app/home/page.tsx
+++ Client/turismo-app-no-country/app/home/page.tsx
@@ -1,9 +1,13 @@
 import { IconsButtonsList } from '../_ui/IconsButtonsList';
+import { ImageCarousel } from '../_ui/ImageCarousel';
 
 const page = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center">
             <IconsButtonsList />
+            <div className="max-w-[1400px] h-[250px] w-full ">
+                <ImageCarousel />
+            </div>
         </div>
     );
 };
