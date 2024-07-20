52f3496403b945582e7e728a2071c311e82d4757
Franco <francotorrico1999@gmail.com>
2024-07-20 16:25:12 -0300

    Rename Route_Review to RouteReview

--- My-Route/src/main/java/com/myroute/entity/RouteReview.java
+++ My-Route/src/main/java/com/myroute/entity/RouteReview.java
@@ -6,28 +6,23 @@ import lombok.*;
 
 @Entity
 @Table(name="route_review")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Route_Review {
+@Data
+public class RouteReview {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
-
+/*
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private User user;*/
     @Column(nullable = false)
     private Integer reviewRange; // range < 5
-
-    //@Min(value = 0, message = "Range must be between 0 and 5")
-    //@Max(value = 5, message = "Range must be between 0 and 5")
     @Column(nullable = false)
     private String comment;
+    /*
     @ManyToOne
     @JoinColumn(name = "route_id", nullable = false)
-    private Route route;
+    private Route route;*/
 
 
 
