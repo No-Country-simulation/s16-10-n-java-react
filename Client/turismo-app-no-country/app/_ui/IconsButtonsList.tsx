b890c9cc70ca8d9a5d5998d7f8ca28907fa2b36a
Jorge Villafraz <jorge9372@gmail.com>
2024-07-20 17:52:40 -0400

    chore: add changes to implement mobile navigation menu

--- Client/turismo-app-no-country/app/_ui/IconsButtonsList.tsx
+++ Client/turismo-app-no-country/app/_ui/IconsButtonsList.tsx
@@ -1,5 +1,3 @@
-
-
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import TerrainIcon from '@mui/icons-material/Terrain';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
@@ -8,24 +6,20 @@ import ChurchIcon from '@mui/icons-material/Church';
 
 import { IconButtonComponent } from './IconButton';
 
+const iconList = [
+    <BeachAccessIcon />,
+    <TerrainIcon />,
+    <LocationCityIcon />,
+    <ForestIcon />,
+    <ChurchIcon />,
+];
+
 export const IconsButtonsList = () => {
     return (
-        <div className="flex m-3 justify-center gap-x-4">
-            <IconButtonComponent>
-                <BeachAccessIcon />
-            </IconButtonComponent>
-            <IconButtonComponent>
-                <TerrainIcon />
-            </IconButtonComponent>
-            <IconButtonComponent>
-                <LocationCityIcon/>
-            </IconButtonComponent>
-            <IconButtonComponent>
-                <ForestIcon/>
-            </IconButtonComponent>
-            <IconButtonComponent>
-                <ChurchIcon/>
-            </IconButtonComponent>
+        <div className="flex m-3 justify-evenly w-full max-w-[1400px] ">
+            {iconList.map((icon: any) => (
+                <IconButtonComponent key={icon}>{icon}</IconButtonComponent>
+            ))}
         </div>
     );
 };
---
+++ Client/turismo-app-no-country/app/_ui/MobileNav.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Drawer, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import Link from 'next/link';
+
+interface MobileNavProps {
+    navList: any;
+}
+
+export const MobileNav: React.FC<MobileNavProps> = ({ navList }) => {
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
+    };
+    return (
+        <>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                className="text-primary p-4 sm:hidden"
+                onClick={toggleDrawer(true)}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                <div className="flex flex-col justify-between items-end m-5 w-40">
+                    {navList.map((link: any) => (
+                        <Link
+                            href={link.href}
+                            key={link.id}
+                            className="text-primary hover:text-secondary p-4 break-words font-medium no-underline"
+                            onClick={toggleDrawer(false)}
+                        >
+                            {link.title}
+                        </Link>
+                    ))}
+                </div>
+            </Drawer>
+        </>
+    );
+};
