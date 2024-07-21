9f29bb96d27626732db2f2ac8cd176ac415f1cff
Jorge Villafraz <jorge9372@gmail.com>
2024-07-20 22:07:27 -0400

    fix: add icon component key

--- Client/turismo-app-no-country/app/_ui/IconsButtonsList.tsx
+++ Client/turismo-app-no-country/app/_ui/IconsButtonsList.tsx
@@ -7,18 +7,34 @@ import ChurchIcon from '@mui/icons-material/Church';
 import { IconButtonComponent } from './IconButton';
 
 const iconList = [
-    <BeachAccessIcon />,
-    <TerrainIcon />,
-    <LocationCityIcon />,
-    <ForestIcon />,
-    <ChurchIcon />,
+    {
+        id: 1,
+        children: <BeachAccessIcon />,
+    },
+
+    {
+        id: 2,
+        children: <TerrainIcon />,
+    },
+    {
+        id: 3,
+        children: <LocationCityIcon />,
+    },
+    {
+        id: 4,
+        children: <ForestIcon />,
+    },
+    {
+        id: 5,
+        children: <ChurchIcon />,
+    },
 ];
 
 export const IconsButtonsList = () => {
     return (
         <div className="flex m-3 justify-evenly w-full max-w-[1400px] ">
-            {iconList.map((icon: any) => (
-                <IconButtonComponent key={icon}>{icon}</IconButtonComponent>
+            {iconList.map((element: any) => (
+                <IconButtonComponent key={element.id}>{element.children}</IconButtonComponent>
             ))}
         </div>
     );
