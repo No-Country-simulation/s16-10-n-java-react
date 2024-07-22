ac9f6412d98230ba541030d16d86ea06a1b7f27b
Gabriel <loveragabriel20@gmail.com>
2024-07-22 00:23:33 -0300

    creo primeros endpoints para table place

--- My-Route/src/main/java/com/myroute/repository/PlaceRepository.java
+++ My-Route/src/main/java/com/myroute/repository/PlaceRepository.java
@@ -1,14 +1,15 @@
 package com.myroute.repository;
 
+import com.myroute.entity.Enum.PreferenceCategory;
 import com.myroute.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlaceRepository extends JpaRepository <Place, Long> {
-    List<Place> findByName(String name);
-
-
+    Optional<Place> findByName(String name);
+    List<Place> findByPreferenceCategory(PreferenceCategory preferenceCategory);
 }
