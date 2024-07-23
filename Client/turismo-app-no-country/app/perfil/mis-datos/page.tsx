6fc11aa53573c439009d8899ea1332a188b64ec8
Mauricio Paiva <mau.paiva10@gmail.com>
2024-07-23 00:09:05 -0300

    feat: change the structure and styles of 'mis-datos' in the profile & add basics form validations

--- Client/turismo-app-no-country/app/perfil/mis-datos/page.tsx
+++ Client/turismo-app-no-country/app/perfil/mis-datos/page.tsx
@@ -1,38 +1,80 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const MisDatos = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: Object) => console.log(data);
+  console.log(errors);
+
   return (
-    <>
-      <section className="px-2 py-2 m-4 bg-gray-500 flex flex-col gap-4 rounded">
-        <h1 className="m-0 font-light text-lg mt-3">Mis datos</h1>
-        <ul className="list-none p-0 m-0 flex flex-col items-start">
-          <li className="inline-block my-2 bg-gray-700 p-2.5 rounded">
-            Nombre: Ejemplo Nombre
-          </li>
-          <li className="inline-block my-2 bg-gray-700 p-2.5 rounded">
-            Edad: 34
-          </li>
-        </ul>
+    <section className="flex justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-gray-500 flex flex-col items-center gap-8 rounded-lg mt-4 w-4/5 py-6 "
+      >
+        <h1 className="w-full text-center mt-0 text-base">Mis datos</h1>
+        <div className="flex flex-col w-3/5">
+          <label className="mb-4 text-xs">Nombre completo</label>
+          <input
+            className="text-xs p-1.5 rounded border-0 focus:outline-none border border-solid border-gray-700 shadow-sm shadow-black"
+            type="text"
+            placeholder="Nombre completo"
+            {...register("nombre", { required: true })}
+          />
+          {errors.nombre?.type === "required" && (
+            <span>Este campo es requerido</span>
+          )}
+        </div>
+        <div className="flex flex-col w-3/5">
+          <label className="mb-4 text-xs">Fecha de nacimiento</label>
+          <input
+            className="text-xs p-1.5 rounded border-0 focus:outline-none border border-solid border-gray-700 shadow-sm shadow-black"
+            type="date"
+            placeholder="Fecha de nacimiento"
+            {...register("nacimiento", { required: true })}
+          />
+          {errors.nacimiento?.type === "required" && (
+            <span>Este campo es requerido</span>
+          )}
+        </div>
+        <div className="flex flex-col w-3/5">
+          <label className="mb-4 text-xs">Dirección</label>
+          <input
+            className="text-xs p-1.5 rounded border-0 focus:outline-none border border-solid border-gray-700 shadow-sm shadow-black"
+            type="text"
+            placeholder="Dirección"
+            {...register("direccion", { required: true })}
+          />
+          {errors.direccion?.type === "required" && (
+            <span>Este campo es requerido</span>
+          )}
+        </div>
+        <div className="flex flex-col w-3/5">
+          <label className="mb-4 text-xs">Email</label>
+          <input
+            className="text-xs p-1.5 rounded border-0 focus:outline-none border border-solid border-gray-700 shadow-sm shadow-black"
+            type="text"
+            placeholder="Email"
+            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+          />
+          {errors.email?.type === "required" && (
+            <span>Este campo es requerido</span>
+          )}
+          {errors.email?.type === "pattern" && <span>Formato incorrecto</span>}
+        </div>
 
-        <ul className="list-none p-0 m-0 flex flex-col justify-start items-start">
-          <li className="inline-block my-2 bg-gray-700 p-2.5 rounded">
-            Pais: Algun Lugar
-          </li>
-          <li className="inline-block my-2 bg-gray-700 p-2.5 rounded">
-            Región: Más Específico
-          </li>
-        </ul>
-
-        <ul className="list-none p-0 m-0 flex flex-col justify-start items-start">
-          <li className="inline-block my-2 bg-gray-700 p-2.5 rounded">
-            Email: ejemplo@gmail.com
-          </li>
-          <li className="inline-block my-2 bg-gray-700 p-2.5 rounded">
-            Contraseña: ********
-          </li>
-        </ul>
-      </section>
-    </>
+        <input
+          type="submit"
+          value="Guardar datos"
+          className="text-xs w-3/5 py-3 mt-4 bg-gray-600 rounded border-0 focus:outline-none"
+        />
+      </form>
+    </section>
   );
 };
 
