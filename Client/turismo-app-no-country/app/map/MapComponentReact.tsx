70617c214f76e73c0192753d50ee4928e739254b
Jorge Villafraz <jorge9372@gmail.com>
2024-07-26 23:19:30 -0400

    feat: fitBounds added to view all markers

--- Client/turismo-app-no-country/app/map/MapComponentReact.tsx
+++ Client/turismo-app-no-country/app/map/MapComponentReact.tsx
@@ -2,13 +2,20 @@
 
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 const myIcon = L.divIcon({
     className: 'my-icon',
     iconSize: [5, 5],
     iconAnchor: [20, 20],
 });
+
+const MyComponent = () => {
+    const map = useMap();
+    console.log('Mapa actual:', map);
+    map.fitBounds([[51.5, -0.09], [51.3, -0.05]]);
+    return null;
+};
 
 const MapComponentReact = () => {
     return (
@@ -17,8 +24,15 @@ const MapComponentReact = () => {
                 center={[51.505, -0.09]}
                 zoom={13}
                 scrollWheelZoom={true}
-                style={{ height: '400px', width: '100%', maxWidth: '400px' , borderRadius: '10px'}}
+                style={{
+                    height: '400px',
+                    width: '100%',
+                    maxWidth: '400px',
+                    borderRadius: '10px',
+                }}
+                boundsOptions={{ padding: [50, 50] }}
             >
+                <MyComponent />
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -28,7 +42,7 @@ const MapComponentReact = () => {
                         HOLA <br /> Easily customizable.
                     </Popup>
                 </Marker>
-                <Marker position={[51.300, -0.05]} icon={myIcon}>
+                <Marker position={[51.3, -0.05]} icon={myIcon}>
                     <Popup>
                         HOLA <br /> Easily customizable.
                     </Popup>
