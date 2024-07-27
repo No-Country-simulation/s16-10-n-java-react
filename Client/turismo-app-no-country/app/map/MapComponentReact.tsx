8ef9b71cca909ca08c6c9c66983045494a9e2c20
Jorge Villafraz <jorge9372@gmail.com>
2024-07-27 12:51:51 -0400

    feat: add styles to boton icon in home

--- Client/turismo-app-no-country/app/map/MapComponentReact.tsx
+++ Client/turismo-app-no-country/app/map/MapComponentReact.tsx
@@ -21,7 +21,7 @@ const MapComponentReact = () => {
     return (
         <>
             <MapContainer
-                center={[51.505, -0.09]}
+                
                 zoom={13}
                 scrollWheelZoom={true}
                 style={{
