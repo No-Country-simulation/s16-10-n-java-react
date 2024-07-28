c7e015f1867b71b8ce4a00be1443c26a51462d99
Mauricio Paiva <mau.paiva10@gmail.com>
2024-07-28 19:40:39 -0300

    feat: add hover to CardOnboarding and adjust margin

--- Client/turismo-app-no-country/app/_ui/CardOnboarding.tsx
+++ Client/turismo-app-no-country/app/_ui/CardOnboarding.tsx
@@ -17,23 +17,24 @@ interface CardOnboardingProps {
 const CardOnboarding: React.FC<CardOnboardingProps> = ({ card, register }) => {
   return (
     <>
-      <label className="flex gap-3 cursor-pointer">
-        <div className="flex flex-col gap-1 max-w-52 sm:max-w-[250px] lg:max-w-[270px]">
-          <img
-            src={card.src}
-            alt=""
-            className="rounded-xl border border-solid border-secondary w-full"
-          />
-          <h5 className="m-0 font-medium text-xs sm:text-sm">{card.title}</h5>
-        </div>
-      <input
-        type="checkbox"
-        value={card.value}
-        {...register("options")}
-        className="cursor-pointer checked:bg-secondary w-5 h-5 rounded-full m-0 border border-solid border-secondary self-center focus:ring-offset-0 focus:ring-0 focus:outline-none"
-      />
-      </label>
-      {/* Agregar hover al input, arreglar los margenes */}
+      <div className="flex gap-3">
+        <label className="flex gap-3">
+          <div className="flex flex-col gap-1 max-w-52 sm:max-w-[250px] lg:max-w-[270px]">
+            <img
+              src={card.src}
+              alt=""
+              className="rounded-xl border border-solid border-secondary w-full"
+            />
+            <h5 className="m-0 font-medium text-xs sm:text-sm">{card.title}</h5>
+          </div>
+        </label>
+        <input
+          type="checkbox"
+          value={card.value}
+          {...register("options")}
+          className="cursor-pointer checked:bg-secondary w-5 h-5 rounded-full m-0 border border-solid border-secondary self-center focus:ring-offset-0 focus:ring-0 focus:outline-none hover:bg-yellowOrange"
+        />
+      </div>
     </>
   );
 };
