b800cda8dbb0ac06625c909c20df4fcf28ca23f7
Mauricio Paiva <mau.paiva10@gmail.com>
2024-07-31 20:43:48 -0300

    feat: add custom images to onboarding

--- Client/turismo-app-no-country/app/_ui/CardOnboarding.tsx
+++ Client/turismo-app-no-country/app/_ui/CardOnboarding.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { UseFormRegister } from "react-hook-form";
 import { FormValues } from "./FormOnboarding";
+import Image from "next/image";
 
 interface Card {
   id: number;
@@ -20,10 +21,12 @@ const CardOnboarding: React.FC<CardOnboardingProps> = ({ card, register }) => {
       <div className="flex gap-3">
         <label className="flex gap-3">
           <div className="flex flex-col gap-1 max-w-52 sm:max-w-[250px] lg:max-w-[270px]">
-            <img
+            <Image
+              width={200}
+              height={90}
               src={card.src}
               alt=""
-              className="rounded-xl border border-solid border-secondary w-full"
+              className="rounded-xl border border-solid border-secondary w-[200px] h-[90px] sm:w-[250px] sm:h-[110px] lg:w-[270px] lg:h-[122px]  object-cover"
             />
             <h5 className="m-0 font-medium text-xs sm:text-sm">{card.title}</h5>
           </div>
