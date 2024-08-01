dd8868f7ad13874e7f10794094605ed26dd06cb7
Mauricio Paiva <mau.paiva10@gmail.com>
2024-07-31 22:22:01 -0300

    feat: add back button in profile

--- Client/turismo-app-no-country/app/perfil/layout.tsx
+++ Client/turismo-app-no-country/app/perfil/layout.tsx
@@ -1,10 +1,20 @@
+"use client"
 import React from "react";
 import EditIcon from '@mui/icons-material/Edit';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useRouter } from "next/navigation";
 
 const layoutPerfil = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push("/perfil");
+  };
+
   return (
     <div className="min-h-screen">
-      <section className="bg-derby flex flex-col items-center justify-center py-2">
+      <section className="bg-derby flex flex-col items-center justify-center py-2 relative">
+        <ArrowBackIosIcon onClick={handleRedirect} className="text-2xl absolute top-0 left-0 mt-6 ml-6 text-secondary cursor-pointer"/>
         <div className="relative">
           <img
             src="https://previews.123rf.com/images/microphoto1981/microphoto19811710/microphoto1981171000024/88705547-icono-de-perfil-de-avatar-predeterminado-marcador-de-posici%C3%B3n-de-foto-gris-perfil-predeterminado.jpg"
